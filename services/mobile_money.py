716122c2a0b0fded1c2125cc77b4b0a50dc9479b
Brian Raymond <Brianraymond074@gmail.com>
2026-06-19 01:24:42 +0300

    Temporarily disable Africa's Talking

--- services/mobile_money.py
+++ services/mobile_money.py
@@ -1,9 +1,3 @@
-import africastalking
-
-def send_money(phone_number, amount):
-    # Fetch the service dynamically inside the function execution
-    mobile_money = africastalking.Payments 
-    
-    # Your payment logic here...
-    # response = mobile_money.mobile_checkout(...)
-    # return response
+def send_money(phone, amount):
+    print(f"TEST PAYMENT: {phone} {amount}")
+    return True, "Mock Payment Success"
